c3a4976ee7f8d1656dc13fff61eea2c56e127c84
Salman <salman.kulur@gmail.com>
2024-11-22 22:01:07 +0530

    complete ui of sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -26,7 +26,7 @@ export default function SignUp() {
                 <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>SignUp
                 </button>
             </form>
-            <div className='flex gap-2 mt-5'>
+            <div className='flex gap-1 mt-5'>
                 <p>Have an account?</p>
                 <Link to={'/sign-in'}>
                     <span className='text-blue-700'>Sign in</span>
