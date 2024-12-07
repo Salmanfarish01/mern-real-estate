54d6fe8dbe0d9b65773d46100d60394995ec9529
Salman <salman.kulur@gmail.com>
2024-12-07 21:51:41 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { createListing
     , deleteListing
-    // , updateListing
+    , updateListing
     // , getListing
     // , getListings 
     } 
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
-// router.post('/update/:id', verifyToken, updateListing);
+router.post('/update/:id', verifyToken, updateListing);
 // router.get('/get/:id', getListing);
 // router.get('/get', getListings);
 
