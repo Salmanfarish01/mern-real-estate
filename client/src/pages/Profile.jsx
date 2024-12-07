76857ab1c2e4de2e32016d5c2ee8cdcff4dfa060
Salman <salman.kulur@gmail.com>
2024-12-07 21:35:10 +0530

    complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,11 @@
 import { useSelector } from 'react-redux';
-import { useState, useEffect, useRef } from 'react';
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { useRef, useState, useEffect } from 'react';
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from 'firebase/storage';
 import { app } from '../firebase';
 import {
     updateUserStart,
@@ -9,8 +14,6 @@ import {
     deleteUserFailure,
     deleteUserStart,
     deleteUserSuccess,
-    signOutUserFailure,
-    signOutUserSuccess,
     signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
@@ -21,14 +24,18 @@ export default function Profile() {
     const [file, setFile] = useState(undefined);
     const [filePerc, setFilePerc] = useState(0);
     const [fileUploadError, setFileUploadError] = useState(false);
-    const [updateSuccess, setUpdateSuccess] = useState(false);
     const [formData, setFormData] = useState({});
+    const [updateSuccess, setUpdateSuccess] = useState(false);
+    const [showListingsError, setShowListingsError] = useState(false);
+    const [userListings, setUserListings] = useState([]);
     const dispatch = useDispatch();
+
     // firebase storage
     // allow read;
     // allow write: if
     // request.resource.size < 2 * 1024 * 1024 &&
     // request.resource.contentType.matches('image/.*')
+
     useEffect(() => {
         if (file) {
             handleFileUpload(file);
@@ -62,6 +69,7 @@ export default function Profile() {
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
     };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -118,11 +126,51 @@ export default function Profile() {
         }
     };
 
+    const handleShowListings = async () => {
+        try {
+            setShowListingsError(false);
+            const res = await fetch(`/api/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (data.success === false) {
+                setShowListingsError(true);
+                return;
+            }
+
+            setUserListings(data);
+        } catch (error) {
+            setShowListingsError(true);
+        }
+    };
+
+    const handleListingDelete = async (listingId) => {
+        try {
+            const res = await fetch(`/api/listing/delete/${listingId}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                console.log(data.message);
+                return;
+            }
+
+            setUserListings((prev) =>
+                prev.filter((listing) => listing._id !== listingId)
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
             <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-                <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
+                <input
+                    onChange={(e) => setFile(e.target.files[0])}
+                    type='file'
+                    ref={fileRef}
+                    hidden
+                    accept='image/*'
+                />
                 <img
                     onClick={() => fileRef.current.click()}
                     src={formData.avatar || currentUser.avatar}
@@ -178,7 +226,6 @@ export default function Profile() {
                     Create Listing
                 </Link>
             </form>
-
             <div className='flex justify-between mt-5'>
                 <span
                     onClick={handleDeleteUser}
@@ -186,16 +233,61 @@ export default function Profile() {
                 >
                     Delete account
                 </span>
-                <span
-                    onClick={handleSignOut}
-                    className='text-red-700 cursor-pointer'>
+                <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
                     Sign out
                 </span>
             </div>
+
             <p className='text-red-700 mt-5'>{error ? error : ''}</p>
             <p className='text-green-700 mt-5'>
                 {updateSuccess ? 'User is updated successfully!' : ''}
             </p>
+            <button onClick={handleShowListings} className='text-green-700 w-full'>
+                Show Listings
+            </button>
+            <p className='text-red-700 mt-5'>
+                {showListingsError ? 'Error showing listings' : ''}
+            </p>
+
+            {userListings && userListings.length > 0 && (
+                <div className='flex flex-col gap-4'>
+                    <h1 className='text-center mt-7 text-2xl font-semibold'>
+                        Your Listings
+                    </h1>
+                    {userListings.map((listing) => (
+                        <div
+                            key={listing._id}
+                            className='border rounded-lg p-3 flex justify-between items-center gap-4'
+                        >
+                            <Link to={`/listing/${listing._id}`}>
+                                <img
+                                    src={listing.imageUrls[0]}
+                                    alt='listing cover'
+                                    className='h-16 w-16 object-contain'
+                                />
+                            </Link>
+                            <Link
+                                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                                to={`/listing/${listing._id}`}
+                            >
+                                <p>{listing.name}</p>
+                            </Link>
+
+                            <div className='flex flex-col item-center'>
+                                <button
+                                    onClick={() => handleListingDelete(listing._id)}
+                                    className='text-red-700 uppercase'
+                                >
+                                    Delete
+                                </button>
+                                <Link to={`/update-listing/${listing._id}`}>
+                                    <button className='text-green-700 uppercase'>Edit</button>
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-    )
+    );
 }
